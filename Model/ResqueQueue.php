3dfe4a48659c9d4d960dc3f08d170461354cc1c2
Maikel Koek <maikel@h-o.nl>
2017-09-11 17:36:49 +0200

    [NS] +: Implement missing methods in ResqueQueue model
    Moved from the magento1-phpconnectorlib module

--- Model/ResqueQueue.php
+++ Model/ResqueQueue.php
@@ -14,8 +14,48 @@ class ResqueQueue implements QueueInterface
     /**
      * @inheritdoc
      */
-    public function enqueue($class, $args = null, $trackStatus = false)
+    public function enqueue($class, $args = null, $trackStatus = true)
     {
         return \Resque::enqueue('connector', $class, $args, $trackStatus);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function dequeue($class, $jobId)
+    {
+        return \Resque::dequeue('connector', [$class => $jobId]);
+    }
+
+    /**
+     * @return int|string
+     */
+    public function waitingStatus()
+    {
+        return \Resque_Job_Status::STATUS_WAITING;
+    }
+
+    /**
+     * @param string $jobId
+     * @return int|bool
+     */
+    public function jobStatus($jobId)
+    {
+        $status = new \Resque_Job_Status($jobId);
+
+        return $status->get();
+    }
+
+    /**
+     * @return array
+     */
+    public function statusMapping()
+    {
+        return [
+            \Resque_Job_Status::STATUS_WAITING  => 'Waiting',
+            \Resque_Job_Status::STATUS_RUNNING  => 'Running',
+            \Resque_Job_Status::STATUS_FAILED   => 'Failed',
+            \Resque_Job_Status::STATUS_COMPLETE => 'Complete',
+        ];
     }
 }
