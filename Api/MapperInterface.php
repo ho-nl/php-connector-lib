cc176cdd9085f1f0f987f1b991908378767dba75
Hans Nieser <hans@h-o.nl>
2017-09-19 17:09:58 +0200

    [OPPO-2140] +: Add mapper interface

---
+++ Api/MapperInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Copyright (c) Reach Digital (http://reachditital.nl/)
+ * See LICENSE.txt for license details.
+ */
+
+namespace ReachDigital\PhpConnectorLib\Api;
+
+/**
+ * Interface MapperInterface
+ * @package ReachDigital\PhpConnectorLib\Api
+ */
+interface MapperInterface
+{
+    /**
+     * Map fields to internal object.
+     *
+     * @param $target mixed
+     * @param $payload mixed
+     * @return mixed the modified $target
+     */
+    public function toInternal($target, $payload);
+
+    /**
+     * Map fields to external object.
+     *
+     * @param $target mixed
+     * @param $payload mixed
+     * @return mixed the modified $target
+     */
+    public function toExternal($target, $payload);
+}
