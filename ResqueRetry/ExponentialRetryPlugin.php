744151ac823969deeaf89ae843eb03d30259ea1f
Paul Hachmang <paul@h-o.nl>
2017-10-13 17:07:50 +0200

    PARA-318: Fixed spacing

--- ResqueRetry/ExponentialRetryPlugin.php
+++ ResqueRetry/ExponentialRetryPlugin.php
@@ -7,37 +7,37 @@ namespace ReachDigital\PhpConnectorLib\ResqueRetry;
 
 class ExponentialRetryPlugin extends RetryPlugin {
 
-	/**
-	 * Get the retry delay from the job, defaults to the amount of steps in the defined backoff
-	 * strategy
-	 *
-	 * @param 	\Resque_Job $job
-	 * @return  int retry limit
-	 */
+    /**
+     * Get the retry delay from the job, defaults to the amount of steps in the defined backoff
+     * strategy
+     *
+     * @param     \Resque_Job $job
+     * @return  int retry limit
+     */
     protected function retryLimit(\Resque_Job $job): int
     {
-		return $this->getInstanceProperty($job, 'retryLimit', count($this->backoffStrategy($job)));
-	}
+        return $this->getInstanceProperty($job, 'retryLimit', count($this->backoffStrategy($job)));
+    }
 
-	/**
-	 * Get the retry delay for the job
-	 *
-	 * @param 	\Resque_Job $job
-	 * @return  int retry delay in seconds
-	 */
+    /**
+     * Get the retry delay for the job
+     *
+     * @param     \Resque_Job $job
+     * @return  int retry delay in seconds
+     */
     protected function retryDelay($job): int
     {
-		$backoffStrategy = $this->backoffStrategy($job);
-		$strategySteps = count($backoffStrategy);
+        $backoffStrategy = $this->backoffStrategy($job);
+        $strategySteps = count($backoffStrategy);
 
-		if ($strategySteps <= 0) {
-			return 0;
-		} elseif (($strategySteps - 1) > $job->payload['retry_attempt']) {
-			return $backoffStrategy[$job->payload['retry_attempt']];
-		} else {
-			return $backoffStrategy[$strategySteps - 1];
-		}
-	}
+        if ($strategySteps <= 0) {
+            return 0;
+        } elseif (($strategySteps - 1) > $job->payload['retry_attempt']) {
+            return $backoffStrategy[$job->payload['retry_attempt']];
+        } else {
+            return $backoffStrategy[$strategySteps - 1];
+        }
+    }
 
     /**
      * Get the backoff strategy from the job, defaults to:
@@ -53,9 +53,9 @@ class ExponentialRetryPlugin extends RetryPlugin {
      *
      * @return  array retry limit
      */
-	protected function backoffStrategy(\Resque_Job $job): array
+    protected function backoffStrategy(\Resque_Job $job): array
     {
-		$defaultStrategy = [0, 60, 600, 3600, 10800, 21600, 43200];
-		return $this->getInstanceProperty($job, 'backoffStrategy', $defaultStrategy);
-	}
+        $defaultStrategy = [0, 60, 600, 3600, 10800, 21600, 43200];
+        return $this->getInstanceProperty($job, 'backoffStrategy', $defaultStrategy);
+    }
 }
--- ResqueRetry/RetryPlugin.php
+++ ResqueRetry/RetryPlugin.php
@@ -19,85 +19,85 @@ class RetryPlugin {
      *
      * @throws \Resque_Exception
      */
-	public function onFailure(\Exception $exception, \Resque_Job $job) {
-		if ($this->retryCriteriaValid($exception, $job)) {
-			$this->tryAgain($exception, $job);
+    public function onFailure(\Exception $exception, \Resque_Job $job) {
+        if ($this->retryCriteriaValid($exception, $job)) {
+            $this->tryAgain($exception, $job);
             $job->payload['retry_key'] = $this->redisRetryKey($job);
-		} else {
-			$this->cleanRetryKey($job);
-		}
-	}
+        } else {
+            $this->cleanRetryKey($job);
+        }
+    }
 
-	/**
-	 * Hook into before the job is performed
-	 *
-	 * Sets up the tracking of the of the amount of attempts trying to perform this job
-	 * 
-	 * @param 	\Resque_Job 	$job
-	 */
-	public function beforePerform(\Resque_Job $job)
+    /**
+     * Hook into before the job is performed
+     *
+     * Sets up the tracking of the of the amount of attempts trying to perform this job
+     * 
+     * @param     \Resque_Job     $job
+     */
+    public function beforePerform(\Resque_Job $job)
     {
-		// Keep track of the number of retry attempts
-		$retryKey = $this->redisRetryKey($job);
+        // Keep track of the number of retry attempts
+        $retryKey = $this->redisRetryKey($job);
 
-		\Resque::redis()->setnx($retryKey, -1); // set to -1 if key doesn't exist
+        \Resque::redis()->setnx($retryKey, -1); // set to -1 if key doesn't exist
         $job->payload['retry_attempt'] = \Resque::redis()->incr($retryKey);
 
-		// Set basic info on the job
+        // Set basic info on the job
         $job->payload['retry_key'] = $this->redisRetryKey($job);
-	}
+    }
 
-	/**
-	 * Hook into the job having been performed
-	 *
-	 * Cleans up any data we've tracked for retrying now that the job has been successfully 
-	 * performed.
-	 * 
-	 * @param 	\Resque_Job 	$job
-	 */
-	public function afterPerform(\Resque_Job $job)
+    /**
+     * Hook into the job having been performed
+     *
+     * Cleans up any data we've tracked for retrying now that the job has been successfully 
+     * performed.
+     * 
+     * @param     \Resque_Job     $job
+     */
+    public function afterPerform(\Resque_Job $job)
     {
-		$this->cleanRetryKey($job);
-	}
+        $this->cleanRetryKey($job);
+    }
 
 
-	/**
-	 * Retry the job
-	 *
-	 * @param 	\Exception 	$exception 	the exception that caused the job to fail
-	 * @param  	\Resque_Job	$job 		the job that failed and should be retried
-	 */
-	protected function tryAgain(\Exception $exception, \Resque_Job $job)
+    /**
+     * Retry the job
+     *
+     * @param     \Exception     $exception     the exception that caused the job to fail
+     * @param      \Resque_Job    $job         the job that failed and should be retried
+     */
+    protected function tryAgain(\Exception $exception, \Resque_Job $job)
     {
-		$retryDelay = $this->retryDelay($job);
-		
-		$queue = $job->queue;
+        $retryDelay = $this->retryDelay($job);
+        
+        $queue = $job->queue;
         $class = get_class($job->getInstance());
-		$arguments = $job->getArguments();
+        $arguments = $job->getArguments();
 
-		$retryingAt = time() + $retryDelay;
+        $retryingAt = time() + $retryDelay;
 
-		if ($retryDelay <= 0) {
-			\Resque::enqueue($queue, $class, $arguments, true);
-		} else {
-			\ResqueScheduler::enqueueAt($retryingAt, $queue, $class, $arguments);
-		}
+        if ($retryDelay <= 0) {
+            \Resque::enqueue($queue, $class, $arguments, true);
+        } else {
+            \ResqueScheduler::enqueueAt($retryingAt, $queue, $class, $arguments);
+        }
 
-		$job->payload['retrying'] = true;
-		$job->payload['retry_delay'] = $retryDelay;
-		$job->payload['retrying_at'] = $retryingAt;
-	}
+        $job->payload['retrying'] = true;
+        $job->payload['retry_delay'] = $retryDelay;
+        $job->payload['retrying_at'] = $retryingAt;
+    }
 
     /**
      * Clean up the retry attempts information from Redis
      *
      * @param \Resque_Job $job
      */
-	protected function cleanRetryKey(\Resque_Job $job)
+    protected function cleanRetryKey(\Resque_Job $job)
     {
-		$retryKey = $this->redisRetryKey($job);
-		\Resque::redis()->del($retryKey);
-	}
+        $retryKey = $this->redisRetryKey($job);
+        \Resque::redis()->del($retryKey);
+    }
 
     /**
      * Return the redis key used to track retries
@@ -107,18 +107,18 @@ class RetryPlugin {
      *
      * @return string
      */
-	protected function redisRetryKey(\Resque_Job $job): string
+    protected function redisRetryKey(\Resque_Job $job): string
     {
-		$name = [
-			'Job{' . $job->queue .'}'
+        $name = [
+            'Job{' . $job->queue .'}'
         ];
-		$name[] = $job->payload['class'];
-		if(!empty($job->payload['args'])) {
-			$name[] = md5(json_encode($job->payload['args']));
-		}
+        $name[] = $job->payload['class'];
+        if(!empty($job->payload['args'])) {
+            $name[] = md5(json_encode($job->payload['args']));
+        }
 
-		return 'retry:' . '(' . implode(' | ', $name) . ')';
-	}
+        return 'retry:' . '(' . implode(' | ', $name) . ')';
+    }
 
     /**
      * Test whether the retry criteria are valid
@@ -129,19 +129,19 @@ class RetryPlugin {
      * @return  boolean
      * @throws \Resque_Exception
      */
-	protected function retryCriteriaValid(\Exception $exception, \Resque_Job $job): bool
+    protected function retryCriteriaValid(\Exception $exception, \Resque_Job $job): bool
     {
-	    $jobInstance = $job->getInstance();
-	    if (! $jobInstance instanceof JobRetryInterface) {
-	        return false;
+        $jobInstance = $job->getInstance();
+        if (! $jobInstance instanceof JobRetryInterface) {
+            return false;
         }
 
-		if ($this->retryLimitReached($job)) {
-		    return false;
+        if ($this->retryLimitReached($job)) {
+            return false;
         }
 
         return $this->retryException($exception, $jobInstance);
-	}
+    }
 
     /**
      * Check whether this exception should be retried. Will retry all exceptions
@@ -152,80 +152,80 @@ class RetryPlugin {
      *
      * @return bool
      */
-	protected function retryException(\Exception $exception, JobRetryInterface $job): bool
+    protected function retryException(\Exception $exception, JobRetryInterface $job): bool
     {
-		foreach ($job->retryExceptions() as $exceptionClass) {
-			if (is_a($exception, $exceptionClass)) {
-			    return true;
+        foreach ($job->retryExceptions() as $exceptionClass) {
+            if (is_a($exception, $exceptionClass)) {
+                return true;
             }
-		}
+        }
 
-		// if we reached this point, the exception is not one we want to retry
-		return false;
-	}
+        // if we reached this point, the exception is not one we want to retry
+        return false;
+    }
 
-	/**
-	 * Check whether the retry limit has been reached
-	 *
-	 * @param  	\Resque_Job	$job
-	 * @return  boolean
-	 */
-	protected function retryLimitReached(\Resque_Job $job): bool
+    /**
+     * Check whether the retry limit has been reached
+     *
+     * @param      \Resque_Job    $job
+     * @return  boolean
+     */
+    protected function retryLimitReached(\Resque_Job $job): bool
     {
-		$retryLimit = $this->retryLimit($job);
+        $retryLimit = $this->retryLimit($job);
 
-		if ($retryLimit === 0) {
-			return true;
-		} elseif ($retryLimit > 0) {
-		    if (!isset($job->payload['retry_attempt'])) {
-		        return true;
+        if ($retryLimit === 0) {
+            return true;
+        } elseif ($retryLimit > 0) {
+            if (!isset($job->payload['retry_attempt'])) {
+                return true;
             }
-			return ($job->payload['retry_attempt'] >= $retryLimit);
-		} else {
-			return false;
-		}
-	}
+            return ($job->payload['retry_attempt'] >= $retryLimit);
+        } else {
+            return false;
+        }
+    }
 
-	/**
-	 * Get the retry delay from the job, defaults to 0
-	 *
-	 * @param 	\Resque_Job 	$job
-	 * @return  int 		retry delay in seconds
-	 */
-	protected function retryLimit(\Resque_Job $job): int
+    /**
+     * Get the retry delay from the job, defaults to 0
+     *
+     * @param     \Resque_Job     $job
+     * @return  int         retry delay in seconds
+     */
+    protected function retryLimit(\Resque_Job $job): int
     {
-		return $this->getInstanceProperty($job, 'retryLimit', 1);
-	}
+        return $this->getInstanceProperty($job, 'retryLimit', 1);
+    }
 
-	/**
-	 * Get the retry delay from the job, defaults to 0
-	 *
-	 * @param 	\Resque_Job 	$job
-	 * @return  int 		retry delay in seconds
-	 */
-	protected function retryDelay($job): int
+    /**
+     * Get the retry delay from the job, defaults to 0
+     *
+     * @param     \Resque_Job     $job
+     * @return  int         retry delay in seconds
+     */
+    protected function retryDelay($job): int
     {
-		return $this->getInstanceProperty($job, 'retryDelay', 0);
-	}
+        return $this->getInstanceProperty($job, 'retryDelay', 0);
+    }
 
-	/**
-	 * Get a property of the job instance if it exists, otherwise
-	 * the default value for this property. Return null for a property
-	 * that has no default set
+    /**
+     * Get a property of the job instance if it exists, otherwise
+     * the default value for this property. Return null for a property
+     * that has no default set
      * @return mixed
-	 */
-	protected function getInstanceProperty($job, $property, $default = null)
+     */
+    protected function getInstanceProperty($job, $property, $default = null)
     {
-		$instance = $job->getInstance();
+        $instance = $job->getInstance();
 
-		if (method_exists($instance, $property)) {
-			return call_user_func_array([$instance, $property], $job);
-		}
+        if (method_exists($instance, $property)) {
+            return call_user_func_array([$instance, $property], $job);
+        }
 
-		if (property_exists($instance, $property)) {
-			return $instance->{$property};
-		}
+        if (property_exists($instance, $property)) {
+            return $instance->{$property};
+        }
 
-		return $default;
-	}
+        return $default;
+    }
 }
